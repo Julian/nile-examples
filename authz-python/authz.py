2fea26e32bf90689002f92fce0572ddae31f7b6e
Julian Berman <Julian@GrayVines.com>
2022-10-15 13:03:16 -0400

    chore: some cosmetic Python style changes

--- authz-python/authz.py
+++ authz-python/authz.py
@@ -1,16 +1,18 @@
 #! /usr/bin/env python
 
-# pylint: disable=consider-using-f-string
-
+from pathlib import Path
 import os
 import json
 import sys
-import emoji
+
 from dotenv import load_dotenv
+from emoji import emojize
 
 from nile_api import AuthenticatedClient, Client
 from nile_api.api.access import (
-    create_policy, delete_policy, list_policies,
+    create_policy,
+    delete_policy,
+    list_policies,
 )
 from nile_api.api.developers import login_developer
 from nile_api.api.organizations import list_organizations
@@ -20,68 +22,79 @@ from nile_api.models.action import Action
 from nile_api.models.resource import Resource
 from nile_api.models.subject import Subject
 
+GOOD = emojize(":check_mark_button:")
+BAD = emojize(":red_circle:")
+
 load_dotenv(override=True)
-required_params = [
-  "NILE_URL",
-  "NILE_WORKSPACE",
-  "NILE_DEVELOPER_EMAIL",
-  "NILE_DEVELOPER_PASSWORD",
-  "NILE_ORGANIZATION_NAME",
-  "NILE_ENTITY_NAME"
-]
-for param in required_params:
-  if not os.environ.get(param):
-    print(emoji.emojize(':red_circle:') + " Error: missing environment variable {}. See .env.defaults for more info and copy it to .env with your values".format(param))
-    sys.exit(1)
+params = {
+    param: os.environ.get(param)
+    for param in [
+        "NILE_URL",
+        "NILE_WORKSPACE",
+        "NILE_DEVELOPER_EMAIL",
+        "NILE_DEVELOPER_PASSWORD",
+        "NILE_ORGANIZATION_NAME",
+        "NILE_ENTITY_NAME",
+    ]
+}
 
-NILE_URL                = os.environ.get('NILE_URL')
-NILE_WORKSPACE          = os.environ.get('NILE_WORKSPACE')
-NILE_DEVELOPER_EMAIL    = os.environ.get('NILE_DEVELOPER_EMAIL')
-NILE_DEVELOPER_PASSWORD = os.environ.get('NILE_DEVELOPER_PASSWORD')
-NILE_ORGANIZATION_NAME  = os.environ.get('NILE_ORGANIZATION_NAME')
-NILE_ENTITY_NAME        = os.environ.get('NILE_ENTITY_NAME')
+for name, value in params.items():
+    if not value:
+        print(
+            f"{BAD} Error: missing environment variable {name}. See .env.defaults for more info and copy it to .env with your values"  # noqa: E501
+        )
+        sys.exit(1)
 
-f = '../usecases/'+NILE_ENTITY_NAME+'/init/users.json'
+users_path = Path(__file__).parent.parent.joinpath(
+    "usecases",
+    params["NILE_ENTITY_NAME"],
+    "init",
+    "users.json",
+)
 try:
-  users = json.load(open(f))
-except:
-  print(emoji.emojize(':red_circle:') + " could not load {}".format(f))
-  sys.exit(1)
-# Load first user only
-index=0
-NILE_TENANT1_EMAIL = users[0]['email']
-NILE_TENANT_PASSWORD = users[0]['password']
+    contents = users_path.read_text()
+except FileNotFoundError:
+    print(f"{BAD} could not find {users_path}")
+    sys.exit(1)
+else:
+    # Load first user only
+    user, *_ = json.loads(contents)
+
+NILE_TENANT1_EMAIL = user["email"]
+NILE_TENANT_PASSWORD = user["password"]
 
 token = login_developer.sync(
-    client=Client(base_url=NILE_URL),
+    client=Client(base_url=params["NILE_URL"]),
     info=LoginInfo(
-        email=NILE_DEVELOPER_EMAIL,
-        password=NILE_DEVELOPER_PASSWORD,
+        email=params["NILE_DEVELOPER_EMAIL"],
+        password=params["NILE_DEVELOPER_PASSWORD"],
     ),
 )
 
-client = AuthenticatedClient(base_url=NILE_URL, token=token.token)
+client = AuthenticatedClient(base_url=params["NILE_URL"], token=token.token)
 
 organizations = list_organizations.sync(
-    workspace=NILE_WORKSPACE,
+    workspace=params["NILE_WORKSPACE"],
     client=client,
 )
 matching = (
     organization
     for organization in organizations
-    if organization.name == NILE_ORGANIZATION_NAME
+    if organization.name == params["NILE_ORGANIZATION_NAME"]
 )
 org = next(matching, None)
 if org is not None:
-    print(emoji.emojize(':check_mark_button:') + " Mapped organization name {} to ID {}".format(org.name, org.id))
+    print(f"{GOOD} Mapped organization name {org.name} to ID {org.id}")
 else:
-    print(emoji.emojize(":red_circle:") + " Could not map organization name {} to an ID".format(NILE_ORGANIZATION_NAME))
+    print(
+        f"{BAD} Could not map organization name {params['NILE_ORGANIZATION_NAME']} to an ID"  # noqa: E501
+    )
     sys.exit(1)
 
 print("\nPolicies at start:")
 policies_start = list_policies.sync(
     client=client,
-    workspace=NILE_WORKSPACE,
+    workspace=params["NILE_WORKSPACE"],
     org=org.id,
 )
 print([each.name for each in policies_start])
@@ -89,25 +102,30 @@ print([each.name for each in policies_start])
 # Create a new policy
 data = CreatePolicyRequest(
     actions=[Action.DENY],
-    resource=Resource(type=NILE_ENTITY_NAME),
+    resource=Resource(type=params["NILE_ENTITY_NAME"]),
     subject=Subject(email=NILE_TENANT1_EMAIL),
-    )
+)
 print(f"\nCreating new policy {data}.")
 policy = create_policy.sync(
     client=client,
-    workspace=NILE_WORKSPACE,
+    workspace=params["NILE_WORKSPACE"],
     org=org.id,
     json_body=data,
 )
-print(emoji.emojize(':check_mark_button:') + " policy id is {}".format(policy.id))
-
-print("\nPolicies post-create:")
-print(list_policies.sync(client=client, workspace=NILE_WORKSPACE, org=org.id))
+print(f"{GOOD} policy id is {policy.id}")
+print(
+    "\nPolicies post-create:",
+    list_policies.sync(
+        client=client,
+        workspace=params["NILE_WORKSPACE"],
+        org=org.id,
+    ),
+)
 
 # Delete the policy just created
 response = delete_policy.sync_detailed(
     client=client,
-    workspace=NILE_WORKSPACE,
+    workspace=params["NILE_WORKSPACE"],
     org=org.id,
     policy_id=policy.id,
 )
@@ -116,13 +134,15 @@ print("policy id {} is deleted".format(policy.id))
 print("\nPolicies post-delete:")
 policies_end = list_policies.sync(
     client=client,
-    workspace=NILE_WORKSPACE,
+    workspace=params["NILE_WORKSPACE"],
     org=org.id,
 )
 print([each.name for each in policies_end])
 
 if policies_start != policies_end:
-    print(emoji.emojize(':red_circle:') + "Something is wrong, policies at start should equal policies at end")
+    print(
+        f"{BAD} Something is wrong, policies at start should equal policies at end"  # noqa: E501
+    )
     sys.exit(1)
 
 sys.exit(0)
